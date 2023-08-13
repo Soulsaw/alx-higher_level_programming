6061a09b634b867a72bfcb49301c1f1ca102621d
Soulsaw <ssawadogo292@gmail.com>
2023-08-13 14:03:18 -0400

    fixing the first task

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    my_list: Is the list of variables such as integer, string, object
+
+    """
+    for item in my_list:
+        print("{:d}".format(item))
