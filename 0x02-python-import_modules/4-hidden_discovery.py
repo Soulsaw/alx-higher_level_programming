e8dbb980edce49c795185a8695ca7b9f3218d51e
Soulsaw <ssawadogo292@gmail.com>
2023-08-13 06:45:17 -0400

    fix task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    _len = len(dir(hidden_4))
-
-    for i in range(_len):
-        print("{}".format(dir(hidden_4)[i]))
+    import hidden_1
+    names = []
+    names = dir(hidden_1)
+    for name in names:
+        if "__" in name:
+            continue
+        else:
+            print(name)
