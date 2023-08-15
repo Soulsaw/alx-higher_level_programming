5e36cfc42ff8172711b0ac87f9d90f07d4f59532
Soulsaw <ssawadogo292@gmail.com>
2023-08-15 05:59:26 -0400

    come in task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,8 @@ def print_reversed_list_integer(my_list=[]):
     my_list: Is the list of variables such as integer, string, object
 
     """
+    if my_list is None:
+        return
     my_list.reverse()
     for item in my_list:
         print("{:d}".format(item))
