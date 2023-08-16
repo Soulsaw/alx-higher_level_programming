b2f056cfdf8ab5f1bc1ebed7db5461e4a9ad397c
Soulsaw <ssawadogo292@gmail.com>
2023-08-16 19:29:04 -0400

    fixing task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,8 @@ def best_score(a_dictionary):
     """
 
     big_score = None if a_dictionary is None \
-        else a_dictionary[list(a_dictionary)[0]]
+        else a_dictionary[list(a_dictionary)[0]] \
+        if len(a_dictionary.keys()) > 0 else None
     if big_score is None:
         return big_score
     for key, value in a_dictionary.items():
