57d65b6d71ea2ac5eea1cebbdde0ebcc61c9bf26
Soulsaw <ssawadogo292@gmail.com>
2023-08-17 01:45:52 -0400

    fixing task 13

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,11 @@
 def complex_delete(a_dictionary, value):
     """
     """
+    new_dic = {}
     for key, values in a_dictionary.items():
         if values is value:
+            new_dic[key] = values
+    for key, value in new_dic.items():
+        if key in a_dictionary:
             del a_dictionary[key]
     return a_dictionary
