492f71b770d6fe41406560b87bff9018d3c0bc3d
Soulsaw <ssawadogo292@gmail.com>
2023-08-30 04:49:24 -0400

    create the folder in come in the second task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-The class to manipule the square
+This is the class to manipule a Square
 
-Autor: @SOULEYTECH
-Date:30/08/2023
+Author: @SOULEYTECH
+Date: 30/08/2023
+
 """
+
+
 class Square:
     """
-    The class to manipule the square
+    This class permit to manipulate a square
 
-    Autor: @SOULEYTECH
-    Date:30/08/2023
-
+    Author: @SOULEYTECH
+    Date: 30/08/2023
     """
     pass
