4eb3b0b8e29bb1eb644559292996e017f81593f3
Soulsaw <ssawadogo292@gmail.com>
2023-08-29 21:31:38 -0400

    create the folder in come in the second task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """This is the documentation of the Square class
+    Args:
+        size (int): The size of the square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
