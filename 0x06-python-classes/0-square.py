b79e4f6d52f15fc9224f064b752bc2e4360d7674
Soulsaw <ssawadogo292@gmail.com>
2023-08-30 04:36:42 -0400

    create the folder in come in the second task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+The class to manipule the square
+
+Autor: @SOULEYTECH
+Date:30/08/2023
+"""
 class Square:
     """
-    The documentation of the empty square class
+    The class to manipule the square
+
+    Autor: @SOULEYTECH
+    Date:30/08/2023
+
     """
+    pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,10 +2,7 @@
 class Square:
     """This is the documentation of the Square class
     Args:
-        size (int): The size of the square.
-
-    Attributes:
-        size (int): The size of the square.
+        size (object): The size of the square.
     """
     def __init__(self, size):
         self.__size = size
