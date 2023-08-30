49a45f6bcf2abb66ae1d692a90379cd68aebf435
Soulsaw <ssawadogo292@gmail.com>
2023-08-30 05:46:19 -0400

    fixing the task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This class define a Square
+
+Author: @SOULEYTECH
+Date: 30/08/2023
+
+"""
+
+
+class Square:
+    """
+    This class define a square with the attribute size
+
+    Attributes:
+        size (int) : This is the size of the square
+    """
+    def __init__(self, size=0):
+        """
+        This is the documentation of the __init__ method
+
+        Args:
+            size (int) : This is the size of the square
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
