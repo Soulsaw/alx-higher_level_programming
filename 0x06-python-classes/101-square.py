8235cc8737a65bdefa61ca05995f2ab081d0532d
Soulsaw <ssawadogo292@gmail.com>
2023-08-31 02:31:13 -0400

    fixing the task 8

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -66,7 +66,8 @@ class Square:
         :return: Nothing
         """
         if type(value) is tuple and len(value) == 2 and \
-                value[0] >= 0 and value[1] >= 0:
+                type(value[0]) is int and type(value[1]) is int \
+                and value[0] >= 0 and value[1] >= 0:
             self.__position = value
         else:
             raise TypeError("position must be a \
@@ -88,6 +89,8 @@ tuple of 2 positive integers")
         if self.size == 0:
             print()
         else:
+            for g in range(self.position[1]):
+                print()
             for i in range(self.size):
                 for k in range(self.position[0]):
                     print("{}".format(" "), end='')
