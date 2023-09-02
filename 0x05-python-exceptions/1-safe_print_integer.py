8b292d8df016950632174ebac26c3dd45931d1a7
Soulsaw <ssawadogo292@gmail.com>
2023-09-02 19:14:03 -0400

    comming task 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    boolean = False
     try:
         print("{:d}".format(value))
-        boolean = True
-    except ValueError:
-        boolean = False
-    return boolean
+        return True
+    except TypeError:
+        return False
