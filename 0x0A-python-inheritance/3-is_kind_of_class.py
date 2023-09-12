abfabf229cf77275107d65ae75b317feaae951b5
Soulsaw <ssawadogo292@gmail.com>
2023-09-11 21:10:57 -0400

    come in task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,7 +8,7 @@ Date: 11/09/2023
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     if the object is an instance of, or if
     the object is an instance of a class that inherited from
