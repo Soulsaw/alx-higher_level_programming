c0194b6a9aed87da476ce2e09deadf9d575a59fe
Soulsaw <ssawadogo292@gmail.com>
2023-09-13 21:21:36 -0400

    come in the task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This function permit to read in the file
+
+Author: @SOULEYTECH
+Date: 14/09/2023
+
+"""
+
+
+def to_json_string(my_obj):
+    """
+    This function permit to read in the file
+
+    Args:
+    :param filename(str): The is the name of file
+    :return: Nothing
+    """
+   return json.JSONEncoder().encode(my_obj)
