bd02513bf7475772e6c5a45e256216dfa25bee08
Soulsaw <ssawadogo292@gmail.com>
2023-09-18 21:04:43 -0400

    come in task 1

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,10 +4,10 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     def setUp(self):
-        self.b3 = Base(12)
+        self.b3 = Base()
 
     def test_id_b3(self):
-        self.assertEqual(self.b3.id, 12)
+        self.assertEqual(self.b3.id, 1)
 
 
 if __name__ == "__main__":
