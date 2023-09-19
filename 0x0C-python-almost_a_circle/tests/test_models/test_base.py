77eeaeb3f9b5e6a5bb6c08958a65a7836c71e3b9
Soulsaw <ssawadogo292@gmail.com>
2023-09-18 20:28:39 -0400

    come in task 1

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,14 +1,24 @@
 import unittest
 from models.base import Base
+"""This class make the unittest of the class Base
+"""
 
 
 class TestBase(unittest.TestCase):
+    """This is the unittest for the Base class
+    """
     def setUp(self):
+        """This methode permit to create an instance of object
+        """
         self.b3 = Base(12)
 
     def test_id_b3(self):
+        """This methods permit to test the sortie of function
+        """
         self.assertEqual(self.b3.id, 12)
 
 
 if __name__ == "__main__":
+    """The entry point of the program
+    """
     unittest.main()
