fda536bda89c806d539f5ceed6455c44469c8a07
Soulsaw <ssawadogo292@gmail.com>
2023-09-22 22:59:01 -0400

    come in task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,6 +37,8 @@ class Base:
     def to_json_string(list_dictionaries):
         """This function return the string representation of a dictionary
         """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     def save_to_file(cls, list_objs):
