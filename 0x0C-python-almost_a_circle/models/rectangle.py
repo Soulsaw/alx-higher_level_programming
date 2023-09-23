a9fd22d1be374fc7ed17e5e1873ff85f1d9f0960
Soulsaw <ssawadogo292@gmail.com>
2023-09-22 21:05:15 -0400

    fixing update constructor

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,3 +127,17 @@ class Rectangle(Base):
         return "[{}] ({}) {}/{} - {}/{}" \
             .format(self.__class__.__name__, self.id,
                     self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """This method permit to update the constructor
+        With the tuple of arguments
+
+        Args:
+            Is the tuple of arguments
+            :param args(tuple): The arguments
+        """
+        self.id = args[0] if len(args) > 0 else self.id
+        self.width = args[1] if len(args) > 1 else self.width
+        self.height = args[2] if len(args) > 2 else self.height
+        self.x = args[3] if len(args) > 3 else self.x
+        self.y = args[4] if len(args) > 4 else self.y
