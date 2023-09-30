a580e51c50704715454bb1e44e00864a86a63bcf
Soulsaw <ssawadogo292@gmail.com>
2023-09-30 18:29:22 -0400

    task 4

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,7 +10,7 @@ def add_integer(a, b=98):
     """This function permit to add to integer
         :param a(int) : The first integer
         :param b(int) : The second integer"""
-    if type(a) not in [int, float]:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
