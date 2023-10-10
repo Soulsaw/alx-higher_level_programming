0f31a832332b1117b8dedda59633e339169a7951
Soulsaw <ssawadogo292@gmail.com>
2023-10-10 04:40:56 -0400

    fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,8 @@ This scripts add all arguments to a file
 Date:20/09/2023
 """
 import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
