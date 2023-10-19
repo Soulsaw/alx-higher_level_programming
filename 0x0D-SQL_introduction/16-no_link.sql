0a29b3aabbac5ba8c40152ab7449984c66a98530
Soulsaw <ssawadogo292@gmail.com>
2023-10-19 12:30:18 +0000

    fixing the task 9 and 16 bug

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- This script print all row only the name and score
 -- description of the table second_table
-SELECT `score`,`name`
-    FROM `second_table`
-WHERE `name` = 'John'
-ORDER BY `score` DESC;
+SELECT score,name
+    FROM second_table
+HAVING name IS NOT NULL
+ORDER BY score DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -13,5 +13,3 @@ VALUES
 (2,'Alex',3),
 (3,'Bob',14),
 (4,'Gorge',8);
-(5,NULL,81);
-(6,'Gorge',8);
