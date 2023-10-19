9ff656c36cd3c8c6168e9ab302d65810534d8f99
Soulsaw <ssawadogo292@gmail.com>
2023-10-19 12:48:39 +0000

    fixing task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,7 @@
--- This script permit to create the first_table
--- with the in and name like its attributes
-CREATE TABLE IF NOT EXISTS `second_table` (id INT,name VARCHAR(256),score INT);
--- inserting data in the second_table
--- inserting some data
-INSERT INTO `second_table` (id,name,score) VALUES(1,'John',10);
--- inserting some data
-INSERT INTO `second_table` (id,name,score) VALUES(2,'Alex',3);
--- inserting some data
-INSERT INTO `second_table` (id,name,score) VALUES(3,'Bob',14);
--- inserting some data
-INSERT INTO `second_table` (id,name,score) VALUES(4,'Gorge',8);
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
 
