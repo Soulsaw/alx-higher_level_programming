3fae5315fbc132a2b22ab50537a316c2e3cf0e7e
Soulsaw <ssawadogo292@gmail.com>
2023-10-19 23:35:20 +0000

    task 10 to 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,4 @@
 -- list all genres of the show Duxter
 SELECT name FROM tv_genres, tv_show_genres, tv_shows
-WHERE tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id AND tv_shows.title = "Dexter";
+WHERE tv_genres.id = tv_show_genres.genre_id AND tv_shows.id = tv_show_genres.show_id AND tv_shows.title = "Dexter"
+ORDER BY name ASC;
