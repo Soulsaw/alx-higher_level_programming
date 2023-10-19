c85995ed646e08f0654d0d7508d6cdfec90b1973
Soulsaw <ssawadogo292@gmail.com>
2023-10-19 12:46:59 +0000

    fixing task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
 -- This script permit to create the first_table
 -- with the in and name like its attributes
-CREATE TABLE IF NOT EXISTS `second_table`(id INT,name VARCHAR(256),score INT);
+CREATE TABLE IF NOT EXISTS `second_table` (id INT,name VARCHAR(256),score INT);
 -- inserting data in the second_table
 -- inserting some data
-INSERT INTO `second_table`(id,name,score)VALUES(1,'John',10);
+INSERT INTO `second_table` (id,name,score) VALUES(1,'John',10);
 -- inserting some data
-INSERT INTO `second_table`(id,name,score)VALUES(2,'Alex',3);
+INSERT INTO `second_table` (id,name,score) VALUES(2,'Alex',3);
 -- inserting some data
-INSERT INTO `second_table`(id,name,score)VALUES(3,'Bob',14);
+INSERT INTO `second_table` (id,name,score) VALUES(3,'Bob',14);
 -- inserting some data
-INSERT INTO `second_table`(id,name,score)VALUES(4,'Gorge',8);
+INSERT INTO `second_table` (id,name,score) VALUES(4,'Gorge',8);
 
