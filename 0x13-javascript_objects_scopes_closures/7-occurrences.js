8091d7411bc6581433fcdfa3ab9e6fd9db53ace1
Soulsaw <ssawadogo292@gmail.com>
2023-11-16 10:22:26 +0000

    task 7 and 8

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let cpt = 0;
+  for (const element of list) {
+    if (searchElement === element) {
+      cpt++;
+    }
+  }
+  return (cpt);
+};
