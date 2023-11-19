30b5e8ee352f9c24ff87557a3424e6da9c21ba28
Soulsaw <ssawadogo292@gmail.com>
2023-11-19 19:20:25 +0000

    task 8 to 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""The SQL definition of the base class
+"""
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    try:
+        _row = session.query(State.id).filter_by(name=sys.argv[4]).first()
+        print(_row[0])
+    except:
+        print('Not found')
