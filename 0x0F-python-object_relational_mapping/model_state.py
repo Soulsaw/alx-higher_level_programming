ca23725f131ea59e76248bb46e06ed52b172741c
Soulsaw <ssawadogo292@gmail.com>
2023-11-19 17:27:01 +0000

    fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """The SQL definition of the base class
 """
+from sqlalchemy import Column, String, Integer, MetaData
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, String, Integer
-
-Base = declarative_base()
+data = MetaData()
+Base = declarative_base(metadata=data)
 
 
 class State(Base):
     """This class map the State tables
     """
-    __tablename__ = "states"
+    __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
