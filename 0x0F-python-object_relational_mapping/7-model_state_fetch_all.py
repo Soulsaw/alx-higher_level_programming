2fb8b57fe6ddfeaf1c5b6845b028f66e7a9f1678
Soulsaw <ssawadogo292@gmail.com>
2023-11-19 17:10:50 +0000

    fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,7 +16,7 @@ class State(Base):
     name = Column(String(128), nullable=False)
 
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 session = Session()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,5 +12,5 @@ class State(Base):
     """
     __tablename__ = "states"
 
-    id = Column(Integer(11), primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
