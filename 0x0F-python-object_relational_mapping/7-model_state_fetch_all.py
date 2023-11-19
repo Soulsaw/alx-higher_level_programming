8d7827d9583b81c23fdb222e0748b5298b20cc22
Soulsaw <ssawadogo292@gmail.com>
2023-11-19 17:30:47 +0000

    fix

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,19 +2,9 @@
 """The SQL definition of the base class
 """
 import sys
-from sqlalchemy import Column, String, Integer, create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-Base = declarative_base()
-
-
-class State(Base):
-    """This class map the State tables
-    """
-    __tablename__ = "states"
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
