07201620edffbcc097ed345dd76b94206fdbf276
Soulsaw <ssawadogo292@gmail.com>
2023-11-20 06:35:33 +0000

    task 11 to 13

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""The SQL definition of the base class
+"""
+import sys
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    st = State(name='Louisiana')
+    session.add(st)
+    session.commit()
+    print(st.id)
