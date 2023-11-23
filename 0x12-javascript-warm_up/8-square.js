e688758d54dd4a7438386a6444c5fbdb521f2c04
Soulsaw <ssawadogo292@gmail.com>
2023-11-23 14:20:08 +0000

    fix the task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,10 +4,6 @@ if (isNaN(myNumber)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < myNumber; i++) {
-    let first = '';
-    for (let j = 0; j < myNumber; j++) {
-      first += 'x';
-    }
-    console.log(first);
+    console.log('X'.repeat(myNumber));
   }
 }
