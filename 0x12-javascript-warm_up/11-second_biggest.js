f861abde179859c03bc8673f2201bda0c3b90556
Soulsaw <ssawadogo292@gmail.com>
2023-11-23 14:40:35 +0000

    fix task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
-const secondBiggest = 0;
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log(secondBiggest);
+const len = process.argv.length;
+if (len <= 3) {
+  console.log('0');
+} else {
+  let result = [];
+  for (let i = 2; i < len; i++) {
+    result.unshift(process.argv[i]);
+  }
+  result = result.sort().reverse();
+  console.log(result[1]);
 }
