a867257596b8265963fbd8e7f91f41374eeef4b8
Soulsaw <ssawadogo292@gmail.com>
2023-12-04 09:32:22 +0000

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""This function is to finds the peak in the list of integers
+"""
+
+
+def find_peak(list_of_integers):
+    """find the peak elements of the lists
+    """
+    max = list_of_integers[0] if len(list_of_integers) > 0 else None
+    for ele in list_of_integers:
+        if max < ele:
+            max = ele
+    return max
