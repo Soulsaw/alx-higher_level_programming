ba000f85ceac6176216441af61efaf0034045e28
Soulsaw <ssawadogo292@gmail.com>
2023-12-04 08:36:15 +0000

    fix task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # the bin
-curl -sX POST "$1" -d "email: test@gmail.com&subject: I will always be here for PLD"
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
