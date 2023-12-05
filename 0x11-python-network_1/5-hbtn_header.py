3e104b1db7888b491e8e9b8ee8cc4925d02a7dd8
Soulsaw <ssawadogo292@gmail.com>
2023-12-05 17:08:06 +0000

    fix task 9

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     """ Implementation """
     req = sys.argv[1]
     response = requests.get(req)
-    print(response.headers['X-Request-Id'])
+    if response.status_code == 200:
+        print(response.headers['X-Request-Id'])
