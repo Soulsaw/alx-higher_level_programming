d153195df1a976c2d644b5d291520cf2eccc0c41
Soulsaw <ssawadogo292@gmail.com>
2023-12-05 07:32:19 +0000

    come in task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,9 +7,12 @@ if __name__ == "__main__":
     """ Implementation """
     data = {'q': sys.argv[1] if len(sys.argv) > 1 else ''}
     url = 'http://0.0.0.0:5000/search_user'
-    res = requests.post(url=url, data=data)
-    json = res.json()
-    if len(json) == 0:
-        print('No result')
-    else:
-        print('[{}] {}'.format(json['id'], json['name']))
+    try:
+        res = requests.post(url=url, data=data)
+        json = res.json()
+        if not json:
+            print('No result')
+        else:
+            print('[{}] {}'.format(json['id'], json['name']))
+    except ValueError:
+        print('Not a valid JSON')
