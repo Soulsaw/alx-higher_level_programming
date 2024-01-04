ac77dd7525bfd0a25a695bee14d063e5158010a8
Soulsaw <ssawadogo292@gmail.com>
2024-01-04 09:53:15 +0000

    fix all bug

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,10 +2,9 @@
 const request = require('request');
 const url = process.argv[2];
 const characterId = 18;
-request(url, function (error, response, body) {
+request(url, { json: true }, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const resp = JSON.parse(body);
-    const movieWithWedgeAntille = resp.results.filter((movie) =>
+    const movieWithWedgeAntille = body.results.filter((movie) =>
       movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
     );
     console.log(movieWithWedgeAntille.length);
