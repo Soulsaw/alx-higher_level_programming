731e22b068280c26cf116286ba9ecd0d8594a260
Soulsaw <ssawadogo292@gmail.com>
2024-01-04 09:39:04 +0000

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,10 +4,10 @@ const url = process.argv[2];
 const characterId = 18;
 request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    const resp = JSON.parse(body)
-    const movieWithWedgeAntille = resp.results.filter((movie)=>
-    movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    )
+    const resp = JSON.parse(body);
+    const movieWithWedgeAntille = resp.results.filter((movie) =>
+      movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+    );
     console.log(movieWithWedgeAntille.length);
   }
 });
