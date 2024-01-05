ced449b3e9955e62731956ca24ebbd31a53597ad
Soulsaw <ssawadogo292@gmail.com>
2024-01-05 08:35:31 +0000

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
+request(url, { json: true }, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const results = body.results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/')) ? count + 1 : count;
+    }, 0));
+  }
+});
+/* const request = require('request');
+const url = process.argv[2];
 const characterId = 18;
 request(url, { json: true }, function (error, response, body) {
   if (!error && response.statusCode === 200) {
@@ -10,3 +20,4 @@ request(url, { json: true }, function (error, response, body) {
     console.log(movieWithWedgeAntille.length);
   }
 });
+ */
