1aa948d882ed5abd342e40196dd48fb3d28e2e53
Soulsaw <ssawadogo292@gmail.com>
2024-01-05 08:16:59 +0000

    fix all

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,12 +4,19 @@ const userId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${userId}`;
 request(url, { json: true }, function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    body.characters.forEach((character) => {
-      request(character, { json: true }, function (err, resp, bod) {
-        if (!err && resp.statusCode === 200) {
-          console.log(bod.name);
-        }
+    const charactersPromises = body.characters.map((character) => {
+      return new Promise((resolve, reject) => {
+        request(character, { json: true }, function (err, resp, bod) {
+          if (!err && resp.statusCode === 200) {
+            resolve(bod.name);
+          } else {
+            reject(err);
+          }
+        });
       });
     });
+    Promise.all(charactersPromises)
+      .then(characterName => console.log(characterName.join('\n')))
+      .catch(characterErr => console.log(characterErr));
   }
 });
